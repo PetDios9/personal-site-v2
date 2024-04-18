ebf3faa03fe9565b031a65fc342bb482571ee42f
Peter Diosdado <diosdadopeter@gmail.com>
2024-04-17 20:11:19 -0500

    feat: import fonts from google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Header from "../components/Header";
+import AboutMe from "../components/AboutMe";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <AboutMe />
+    </div>
+  );
 }

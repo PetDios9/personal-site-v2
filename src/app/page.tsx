e950ab9e6e0de4d8f895679705bf180d13ab1f88
Peter Diosdado <diosdadopeter@gmail.com>
2024-04-24 22:46:51 -0700

    style: portrait now inline with intro text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import Portrait from "../components/Portrait";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col">
+    <div className="relative flex flex-col bg-black">
       <div className="fixed top-0 z-50">
         <Header />
       </div>
       <div className="sticky top-0 lg:z-40">
         <AboutMe />
       </div>
-      <div className="sticky top-0" id="portrait">
+      <div className="sticky lg:fixed top-0" id="portrait">
         <Portrait />
       </div>
       <div className="sticky top-0" id="projects">

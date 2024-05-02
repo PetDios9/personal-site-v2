858ed33a67f5486b06eea51c9760426daf4841f3
Peter Diosdado <diosdadopeter@gmail.com>
2024-05-02 00:07:52 -0700

    style: replaced textholder text in about me component

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,28 +1,30 @@
 export default function AboutMe() {
   return (
-    <div className="bg-tetriary-color lg:pt-[150px] text-primary-color h-screen flex flex-col justify-start items-left p-9 md:p-16 gap-3 md:gap-8 lg:gap-8 text-pretty sticky top-0">
+    <div className="bg-tetriary-color lg:pt-[150px] text-primary-color h-screen flex flex-col justify-center items-left p-9 md:p-16 gap-3 md:gap-8 lg:gap-8 text-pretty sticky top-0">
       <h1 className=" md:text-3xl italic lg:text-4xl font-medium ">About Me</h1>
-      <h2 className="md:text-3xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Massa vitae tortor
-        condimentum lacinia quis vel eros donec ac. Tellus integer feugiat
-        scelerisque varius morbi enim nunc faucibus a.
+      <h2 className="md:text-2xl">
+        Hi! My name is Peter Diosdado, Full Stack Software Engineer. Currently
+        seeking my next professional role so if you are looking for star
+        engineers, know of any opportunities as a backend, front end, or full
+        stack engineer, or just want to say hello, feel free to reach out to me
+        through the contact button.
       </h2>
       <h2 className="md:text-xl lg:text-3xl font-medium italic">
         Professional
       </h2>
       <p className="md:text-xl font-light">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Imperdiet proin
-        fermentum leo vel. Facilisis leo vel fringilla est ullamcorper eget
-        nulla facilisi.
+        On the front end, I have built beautiful user interfaces using
+        TypeScript, JavaScript, HTML, CSS, Shopify Liquid, React, Next.js and
+        more. On the backend, I have constructed robust & optimized server-side
+        applications with Node, Express.js, Python, Flask, and other in demand
+        technologies. Always ready to learn new languages and their tools to
+        help me grow as a developer.
       </p>
       <h2 className="md:text-xl lg:text-3xl font-medium italic">Personal</h2>
       <p className="md:text-xl font-light">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Imperdiet proin
-        fermentum leo vel. Facilisis leo vel fringilla est ullamcorper eget
-        nulla facilisi.
+        Father of two cats. Love to travel, try new restaurants, take film
+        photos, scour Spotify playlists for new music, and read manga. Currently
+        really into Jujutsu Kaisen, Sakamoto Days, and One Piece.
       </p>
     </div>
   );

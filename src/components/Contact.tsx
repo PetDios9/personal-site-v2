11eb5d8213f789d3131f5a33f7a0b29c15bf8e26
Peter Diosdado <diosdadopeter@gmail.com>
2024-05-04 21:08:06 -0700

    feat: added links to contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,23 +9,31 @@ export default function Contact() {
           {/* <p className=" text-primary-color">Peter</p> */}
         </div>
       </div>
-      <div className="flex flex-col p-9 md:p-16 bg-tetriary-color h-[65vh] w-screen lg:w-[50vw] z-10 text-primary-color gap-3">
-        <h2 className="md:text-xl lg:text-3xl font-medium italic text-center">
-          Phone Number
-        </h2>
-        <p className="md:text-xl font-light text-center">209-479-0926</p>
-        <h2 className="md:text-xl lg:text-3xl font-medium italic text-center">
-          Email
-        </h2>
-        <p className="md:text-xl font-light text-center">
-          peterdiosdadochavez@gmail.com
-        </p>
-        <h2 className="md:text-xl lg:text-3xl font-medium italic text-center">
-          LinkedIn
-        </h2>
-        <p className="md:text-xl font-light text-center">
-          https://linkedin.com/in/peter-diosdado
-        </p>
+      <div className="flex flex-col align-middle pt-20 bg-tetriary-color h-[65vh] w-screen lg:w-[50vw] z-10 text-primary-color gap-12">
+        <a href="tel:12094790926">
+          <h2 className="text-3xl font-medium italic text-center">
+            Phone Number
+          </h2>
+          <p className="text-xl font-light text-center">209-479-0926</p>
+        </a>
+        <a href="mailto:peterdiosdadochavez@gmail.com">
+          <h2 className="text-3xl font-medium italic text-center">Email</h2>
+          <p className="text-xl font-light text-center">
+            peterdiosdadochavez@gmail.com
+          </p>
+        </a>
+        <a href="https://linkedin.com/in/peter-diosdado" target="_blank">
+          <h2 className="text-3xl font-medium italic text-center">LinkedIn</h2>
+          <p className="text-xl font-light text-center">
+            https://linkedin.com/in/peter-diosdado
+          </p>
+        </a>
+        <a href="https://github.com/petdios9" target="_blank">
+          <h2 className="text-3xl font-medium italic text-center">LinkedIn</h2>
+          <p className="text-xl font-light text-center">
+            https://github.com/petdios9
+          </p>
+        </a>
       </div>
     </div>
   );

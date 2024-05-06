06d0b22eea0e209f00210ee08e468d5ea927850b
Peter Diosdado <diosdadopeter@gmail.com>
2024-05-06 00:37:08 -0700

    style: changed contact headers to symbols

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,30 +7,48 @@ export default function Contact() {
         <h2 className="text-primary-color">Say Hi!</h2>
         <div className="rounded-full flex bg-tetriary-color size-48 border-4 bg-[url('/avatar.png')] bg-center bg-cover"></div>
       </div>
-      <div className="flex flex-col align-middle justify-center bg-tetriary-color h-[65vh] w-screen lg:w-[50vw] z-10 text-primary-color gap-10">
-        <a href="tel:12094790926">
-          <h2 className="text-3xl font-medium italic text-center">
-            Phone Number
-          </h2>
-          <p className="text-xl font-light text-center">209-479-0926</p>
+      <div className="flex flex-col items-center justify-center bg-tetriary-color h-[65vh] w-screen lg:w-[50vw] z-10 text-primary-color gap-8">
+        <a
+          className="flex flex-col items-center justify-center gap-4"
+          href="tel:12094790926"
+        >
+          <div className="rounded-full flex justify-center items-center bg-secondary-color size-11 lg:size-16">
+            <h2 className="text-3xl font-medium text-center">📱</h2>
+          </div>
+          <p className="text-xl lg:text-2xl font-bold text-center">
+            209-479-0926
+          </p>
         </a>
-        <a href="mailto:peterdiosdadochavez@gmail.com">
-          <h2 className="text-3xl font-medium italic text-center">Email</h2>
-          <p className="text-xl font-light text-center">
+        <a
+          className="flex flex-col items-center justify-center gap-4"
+          href="mailto:peterdiosdadochavez@gmail.com"
+        >
+          <div className="rounded-full flex justify-center items-center bg-secondary-color size-11 lg:size-16">
+            <h2 className="text-3xl font-medium text-center">📧</h2>
+          </div>
+          <p className="text-xl lg:text-2xl font-bold text-center">
             peterdiosdadochavez@gmail.com
           </p>
         </a>
-        <a href="https://linkedin.com/in/peter-diosdado" target="_blank">
-          <h2 className="text-3xl font-medium italic text-center">LinkedIn</h2>
-          <p className="text-xl font-light text-center">
-            https://linkedin.com/in/peter-diosdado
-          </p>
+        <a
+          className="flex flex-col items-center justify-center gap-4"
+          href="https://linkedin.com/in/peter-diosdado"
+          target="_blank"
+        >
+          <div className="rounded-full flex justify-center items-center bg-secondary-color size-11 lg:size-16">
+            <h2 className="text-3xl font-medium text-center">💻</h2>
+          </div>
+          <p className="text-xl lg:text-2xl font-bold text-center">LinkedIn</p>
         </a>
-        <a href="https://github.com/petdios9" target="_blank">
-          <h2 className="text-3xl font-medium italic text-center">Github</h2>
-          <p className="text-xl font-light text-center">
-            https://github.com/petdios9
-          </p>
+        <a
+          className="flex flex-col items-center justify-center gap-4"
+          href="https://github.com/petdios9"
+          target="_blank"
+        >
+          <div className="rounded-full flex justify-center items-center bg-secondary-color size-11 lg:size-16">
+            <h2 className="text-3xl font-medium text-center">👨🏽‍💻</h2>
+          </div>
+          <p className="text-xl lg:text-2xl font-bold text-center">Github</p>
         </a>
       </div>
     </div>

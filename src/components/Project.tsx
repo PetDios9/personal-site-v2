5817ee1bce10ef7172dfd518c8b543361e03ea7b
Peter Diosdado <diosdadopeter@gmail.com>
2024-05-06 22:47:07 -0700

    feat: added working project component and links to project section

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,17 +1,22 @@
+import Image, { StaticImageData } from "next/image";
+
 export default function Project({
-  bgImageURL,
+  bgImage,
   projectName,
+  projectLink,
 }: {
-  bgImageURL: string;
+  bgImage: StaticImageData;
   projectName: string;
+  projectLink: string;
 }) {
   return (
-    <div
-      className={`lg:hover:scale-110 bg-[${bgImageURL}] bg-cover w-full bg-center h-full cursor-pointer transform transition duration-300 lg:hover:z-10`}
-    >
-      <div className="h-full w-auto bg-overlay flex items-center justify-center">
-        <p className="text-tetriary-color">{projectName}</p>
-      </div>
+    <div className="relative lg:hover:scale-110 w-full h-full cursor-pointer transform transition duration-300 lg:hover:z-10">
+      <a href={projectLink} target="_blank">
+        <Image src={bgImage} alt="Logo" fill className="-z-10 object-cover" />
+        <div className="h-full w-auto bg-overlay flex items-center justify-center">
+          <p className="text-tetriary-color">{projectName}</p>
+        </div>
+      </a>
     </div>
   );
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,33 @@
 /* // eslint-disable quotes  */
 import Project from "./Project";
+import foxybae from "../../public/foxybae.jpg";
+import luna from "../../public/luna.jpeg";
+import favoriteTracks from "../../public/favoritetracks.png";
+import wedding from "../../public/wedding.png";
+
 export default function Projects() {
   return (
     <div className="bg-secondary-color grid grid-cols-2 gap-0 place-items-center h-screen w-screen lg:w-[50vw]">
-      <Project bgImageURL="url('/IMG_5479.jpg')" projectName="Peter" />
-      <Project bgImageURL="url('/IMG_5479.jpg')" projectName="George" />
-      <Project bgImageURL="url('/IMG_5479.jpg')" projectName="Peter" />
-      <Project bgImageURL="url('/IMG_5479.jpg')" projectName="Peter" />
-      <Project bgImageURL="url('/IMG_5479.jpg')" projectName="Peter" />
-      <Project bgImageURL="url('/IMG_5479.jpg')" projectName="Peter" />
+      <Project
+        bgImage={foxybae}
+        projectName="FoxyBae"
+        projectLink="https://foxybae.com"
+      />
+      <Project
+        bgImage={luna}
+        projectName="Luna Blanket"
+        projectLink="https://lunablanket.com"
+      />
+      <Project
+        bgImage={favoriteTracks}
+        projectName="Your Favorite Tracks"
+        projectLink="https://thunderous-bienenstitch-0bfa22.netlify.app/"
+      />
+      <Project
+        bgImage={wedding}
+        projectName="Dorryen and Mel"
+        projectLink="https://quirky-swanson-52636b.netlify.app"
+      />
     </div>
   );
 }
